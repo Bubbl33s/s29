2774dca55bf56dbae2539e0beccaa6510f80c629
Bubbl33s <jairo.lf.8@gmail.com>
2023-12-11 18:58:36 -0500

    modulos

---
+++ modulos.js
@@ -0,0 +1,10 @@
+// FUNCIÓN QUE MUESTRA LOS 10 PRIMEROS NÚMEROS
+function contar() {
+    // for (inicio, final, pasos)
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+// EXPORTAMOS LA FUNCIÓN
+export {contar};
